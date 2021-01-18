04d74fac397fef1ff054eeb728e0296772793822
Marko Zaric <bleja997@gmail.com>
2021-01-18 22:55:11 +0100

    Implement the filtered article search

--- src/controlers/api/article.controller.ts
+++ src/controlers/api/article.controller.ts
@@ -16,6 +16,7 @@ import * as sharp from 'sharp';
 import { EditArticleDto } from "src/dtos/article/edit.article.dto";
 import { RoleCheckedGuard } from "src/misc/role.checker.guard";
 import { AllowToRoles } from "src/misc/allow.to.roles.descriptor";
+import { ArticleSearchDto } from "src/dtos/article/article.search.dto";
 @Controller('api/article')
 @Crud({
     model:{
@@ -222,4 +223,10 @@ export class ArticleController{
 
         return new ApiResponse('ok', 0, 'One photo deleted!'); 
         }
+    @Post('search')
+    @UseGuards(RoleCheckedGuard)
+    @AllowToRoles('administrator', 'user')
+    async search(@Body() data: ArticleSearchDto): Promise<Article[]>{
+        return await this.service.search(data);
+    }
 }
--- src/services/article/article.service.ts
+++ src/services/article/article.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
+import { features } from "process";
 import { ArticleFeature } from "src/controlers/entities/article-feature.entity";
 import { ArticlePrice } from "src/controlers/entities/article-price.entity";
 import { Article } from "src/controlers/entities/article.entity";
 import { AddArticleDto } from "src/dtos/article/add.article.dto";
+import { ArticleSearchDto } from "src/dtos/article/article.search.dto";
 import { EditArticleDto } from "src/dtos/article/edit.article.dto";
 import { ApiResponse } from "src/misc/Api.Response.class";
-import { Repository } from "typeorm";
+import { Any, Repository } from "typeorm";
 
 @Injectable()
 export class ArticleService extends TypeOrmCrudService<Article> {
@@ -110,8 +112,92 @@ export class ArticleService extends TypeOrmCrudService<Article> {
             "category",
             "articleFeatures",
             "features",
-            "articlePrices"
+            "articlePrices",
+            "photos",
         ]
    });
     }
-}}
+
+}
+async search(data: ArticleSearchDto): Promise<Article[]>{ 
+    const builder = await this.article.createQueryBuilder("article");
+
+    builder.innerJoinAndSelect(
+        "article.articlePrices",
+        "ap", 
+        "ap.createdAt = (SELECT MAX(ap.created_at) FROM article_price AS ap WHERE ap.article_id = article.article_id)"
+        );
+    builder.leftJoinAndSelect("article.articleFeatures", "af");
+
+
+    builder.where('article.categoryId = :categoryId', {categoryId: data.categoryId});
+    if (data.keywords && data.keywords.length> 0 ){
+        builder.andWhere(`(article.name LIKE : kw OR article.excerpt LIKE kw OR article.description LIKE kw)`, {kw: '%'+ data.keywords.trim() + '%'});
+    }
+
+    if (data.priceMin && typeof data.priceMin  === 'number'){
+        builder.andWhere('ap.price >= :min', {min: data.priceMin});   
+    }
+    if (data.priceMax && typeof data.priceMax  === 'number'){
+        builder.andWhere('ap.price >= :max', {max: data.priceMax});   
+    }
+
+    if (data.features.length > 0 ){
+        for (const feature of data.features){
+            builder.andWhere('af.featureId = :fid AND af.value IN (:fVals)',
+            {
+                fId: feature.featureId,
+                fVals :feature.values,
+            }
+            );
+        }
+    }
+    let orderBy = 'name';
+    let orderDirection: 'ASC' | 'DESC' = 'ASC';
+
+    if (data.orderBy){
+        orderBy = data.orderBy;
+        if (orderBy === 'price'){
+            orderBy = 'ap.price';
+        }
+
+        if (orderBy === 'name'){
+            orderBy = 'article.name';
+        }
+    }
+    if (data.orderDirection){
+        orderDirection = data.orderDirection;
+    }
+
+    builder.orderBy(orderBy , orderDirection);
+
+    let page = 0;
+    let perPage: 5 | 10 | 25 | 50 | 75 = 25;
+
+    if (data.page && typeof data.page === 'number'){
+        page = data.page;
+    }
+
+    if (data.itemsPerPage && typeof data.itemsPerPage === 'number'){
+        perPage = data.itemsPerPage;
+    }
+
+    builder.skip(page * perPage);
+    builder.take(perPage);
+
+    let articleIds = await (await builder.getMany()).map(article => article.articleId);
+
+    return await this.article.find({
+        where: {articleId: Any(articleIds)},
+        relations:[
+            "category",
+            "articleFeatures",
+            "features",
+            "articlePrices",
+            "photos",
+        ]
+    });
+}
+}
+
+
